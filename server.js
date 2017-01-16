4841894f3b7839490272071b60975059ebda8dc5
GloryRoadWang <tyutwang@gmail.com>
2017-01-16 17:04:01 +0800

    index以及server功能调整

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 var http = require('http');
 var url = require('url');
 
-function start(){
+function start(route, handle){
 
     function onRequest(request, response){
+        var postData = '';
         var pathname = url.parse(request.url).pathname;
-        console.log('Request for ' + pathname + ' received!');
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World!");
-        response.end();
+        console.log("Request for " + pathname + " received.");
+
+        request.setEncoding("utf8");
+
+        // var content = route(handle, pathname, response);
+
+        // response.writeHead(200, {"Content-Type": "text/plain"});
+        // response.write(content);
+        // response.end();
+
+        request.addListener("data", function(postDataChunck){
+            postData += postDataChunck;
+            console.log("received POST data chuck '" + postDataChunck +"'.");
+        })
+
+        request.addListener("end", function(){
+            route(handle, pathname, response, postData);
+        })
+
+        // route(handle, pathname, response);
+
     }
+
     http.createServer(onRequest).listen(8888);
     console.log('server has started!');
 }
